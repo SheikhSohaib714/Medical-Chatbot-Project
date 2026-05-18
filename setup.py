3b78b69d5d1033ddefc4b0f8fa05a5100a217f80
SheikhSohaib714 <eyelens714@gmail.com>
2026-05-18 16:45:45 +0600

    setup file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.1',
+    author='Sheikh_Sohaib',
+    author_email="eyelens714@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
